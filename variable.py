f3c43976d6af8327401b3cd94aab2f387b7cde44
Rehan <rehanbinazim16@gmail.com>
2025-04-28 00:35:21 +0600

    boolean

--- variable.py
+++ variable.py
@@ -1,16 +1,29 @@
 #variable and taking input from user and print it.
 # also use f string for printing
 
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-email = input("Enter your email address: ")
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# email = input("Enter your email address: ")
 
-print(f"Hello " + first_name + " " + last_name)
-print(f"Hello {first_name} {last_name} {email}")
+# print(f"Hello " + first_name + " " + last_name)
+# print(f"Hello {first_name} {last_name} {email}")
 
 #integers
 
-age = 25
-quantity = 4
+# age = 25
+# quantity = 4
 
-print(f"Age: {age}, Quantity: {quantity}")	
+# print(f"Age: {age}, Quantity: {quantity}")	
+
+is_student = True
+for_sale = False
+
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")     
+    
+if for_sale:
+    print("The item is for sale.")
+else:
+    print("The item is not for sale.")  
