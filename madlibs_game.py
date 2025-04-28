7a6998a60fab0d02c7c15ce224a5e94ef18885ee
Rehan Bin Azim <rehanbinazim16@gmail.com>
2025-04-28 23:29:37 +0600

    made a game named madlibs. It is too much fungit add .!

---
+++ madlibs_game.py
@@ -0,0 +1,26 @@
+#madlibs game
+#word game where you fill in the blanks with random words to create a story
+
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")     
+adjective3 = input("Enter a third adjective: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+noun3 = input("Enter a third noun: ")
+noun4 = input("Enter a fourth noun: ")
+noun5 = input("Enter a fifth noun: ")
+noun6 = input("Enter a sixth noun: ")
+noun7 = input("Enter a seventh noun: ")
+adjective4 = input("Enter a fourth adjective: ")
+adjective5 = input("Enter a fifth adjective: ")
+adjective6 = input("Enter a sixth adjective: ")
+adjective7 = input("Enter a seventh adjective: ")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In the zoo, I saw a {adjective2} {noun1} jumping up and down in its tree.") 
+print(f"I saw a {adjective3} {noun2} flying around the zoo in its {noun3}.")
+print(f"Mary had a little {noun4} that was as {adjective4} as snow.")
+print(f"Then I found a {noun5} that was {adjective5} and {adjective6}.")
+print(f"I went to the {noun6} and saw a {adjective7} {noun7}.")
+print("I was happy to see it.")
