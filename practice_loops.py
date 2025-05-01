756f2cd66c4abc0ab271f26420a7857dfd71d9e7
Rehan Bin Azim <rehanbinazim16@gmail.com>
2025-05-01 10:10:18 +0600

    again practicing...

--- practice_loops.py
+++ practice_loops.py
@@ -2,11 +2,25 @@
 # list1 = [1,2,3,4,5] 
 #output: 1 2 3 4 5
 
-print("Enter 5 numbers:")
-list1 = []
-for i in range(5):
-    list = int(input()) 
-    list1.append(list)
-print(list1)
+# print("Enter 5 numbers:")
+# list1 = []
+# for i in range(5):
+#     list = int(input()) 
+#     list1.append(list)
+# print(list1)
 
 # print(help(list1))
+
+
+# 1 2 3 4 5 
+# nums = list(map(int, input("Enter 5 numbers: ").split()))
+# print(nums)
+
+n = int(input("Enter range number: "))
+list = []
+for i in range(n):
+    num = int(input("Enter numbers: "))
+    list.append(num)
+    
+print(f"The numbers are: {list}")
+    
